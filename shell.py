3d83a7d2e87c52ce3b48cf9fb9117da0610ce495
dolliars <dolly.lanuza@gmail.com>
2019-04-27 19:31:56 -0400

    Now with pipe access

--- shell.py
+++ shell.py
@@ -3,15 +3,26 @@
 import os
 import sys
 
+def exec_pipeline(line):
+    commands = line.split('|')
+    fds = {0:0, 1:1, 2:2}
+
+    for command in commands[:-1]:
+        (r, w) = os.pipe()
+        fds[1] = w
+        exec_redirected(commands.split(), fds)
+        fds[0] = r
+
+    fds[1] = 1
+    pid = exec_redirected(commands[-1].split(), fds)
+    return os.waitpid(pid,0)
+
 while True:
     print("$~\(-.-)/~$")
     sys.stdout.flush
     line = sys.stdin.readline()
     args = line.split()
-    print(args) 
-
-    #env = os.environ['HOME']
-
+    
     if args[0] == 'cd':
         os.chdir(args[1])
     else:
@@ -20,3 +31,4 @@ while True:
         if pid == 0: 
             os.execvp(args[0], args)
         os.waitpid(pid, 0)
+
