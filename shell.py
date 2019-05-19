7f3a85959f9f45906236c0fea726a9b2ede49a78
dolliars <dolly.lanuza@gmail.com>
2019-05-19 11:15:42 -0400

    Progress has been made

--- shell.py
+++ shell.py
@@ -3,32 +3,52 @@
 import os
 import sys
 
+def exec_redirected(args, fds):
+    pid = os.fork()
+    if pid == 0:
+        for (childfd, parentfd) in fds.items():
+            os.dup2(parentfd, childfd)
+        os.execvp(args[0], args)
+    return pid
+
 def exec_pipeline(line):
     commands = line.split('|')
-    fds = {0:0, 1:1, 2:2}
+    fds = {0:0, 1:1, 2:2} # mapping
 
     for command in commands[:-1]:
         (r, w) = os.pipe()
         fds[1] = w
-        exec_redirected(commands.split(), fds)
+        exec_redirected(command.split(), fds)
+        os.close(w)
+        if fds[0] != 0:
+            os.close(fds[0])
         fds[0] = r
 
     fds[1] = 1
     pid = exec_redirected(commands[-1].split(), fds)
+    if fds[0] != 0:
+        os.close(fds[0])
     return os.waitpid(pid,0)
 
-while True:
-    print("$~\(-.-)/~$")
-    sys.stdout.flush
-    line = sys.stdin.readline()
-    args = line.split()
-    
-    if args[0] == 'cd':
-        os.chdir(args[1])
-    else:
-        pid = os.fork()
+def exec_shell():
+    while True:
+        print("$~\(-.-)/~$")
+        sys.stdout.flush
+        line = sys.stdin.readline() # ls | sort
+        args = line.split()         # ['ls', '|', 'sort']
         
-        if pid == 0: 
-            os.execvp(args[0], args)
-        os.waitpid(pid, 0)
+        if args[0] == 'cd':
+            os.chdir(args[1])
+        else:
+            #exec_pipeline(line)
+            pid = os.fork()         
+            print('pid:', pid)
+            if pid == 0:
+                os.execvp(args[0], args)
+            os.waitpid(pid, 0)
+
+def main ():
+    exec_shell()
+
+main()
 
